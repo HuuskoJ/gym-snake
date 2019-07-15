87a808ef740a7277fa17f4ac288a0fbe89722baa
Jaakko Huusko <36589130+HuuskoJ@users.noreply.github.com>
2019-07-15 10:54:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
-setup(name="gym_snake",
-	version='0.0.1',
-      install_requires=['gym', 'pygame', 'numpy']
-	  )
+setup(
+    name="gym_snake",
+    version='0.0.1',
+    install_requires=['gym', 'pygame', 'numpy']
+    )
